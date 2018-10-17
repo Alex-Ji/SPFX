d540e545ae48b7d2684b3b7ab501ff2880bb4005
AlexJi <jikai2008@sina.com>
2018-10-17 15:35:37 +0800

    Create project

---
+++ SPFx webparts/src/webparts/carouselFramework/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICarouselFrameworkWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'CarouselFrameworkWebPartStrings' {
+  const strings: ICarouselFrameworkWebPartStrings;
+  export = strings;
+}
